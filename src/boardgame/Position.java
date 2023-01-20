99cbe3614f46fe2fb742d6661c698930fa4b0f7a
Gabriel Silva <gabriel.s.alves1997@gmail.com>
2023-01-20 14:33:49 -0300

    Camada de xadrez e impressão do tabuleiro

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -10,19 +10,19 @@ public class Position {
 		this.column = column;
 	}
 
-	public int getLinha() {
+	public int getRow() {
 		return row;
 	}
 
-	public void setLinha(int row) {
+	public void setRow(int row) {
 		this.row = row;
 	}
 
-	public int getColuna() {
+	public int getColumn() {
 		return column;
 	}
 
-	public void setColuna(int coluna) {
+	public void setColumn(int coluna) {
 		this.column = coluna;
 	}
 	
