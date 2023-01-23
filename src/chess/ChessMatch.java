5c589d3b71c86bde7c65313adf132d7f28724810
Gabriel Silva <gabriel.s.alves1997@gmail.com>
2023-01-23 11:12:10 -0300

    Movimentos possiveis para a Piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,6 +44,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("Nao existe peca na posicao de origem !");
 		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Nao tem movimentos possiveis para essa peca !");
+		}
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
